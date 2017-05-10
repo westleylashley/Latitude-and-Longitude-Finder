7d24e836908b94007f86c1d3ccb0a8a03d97fee4
weslashley <westleyl@gmail.com>
2017-05-10 10:37:53 -0700

    Updates

--- WhatsMyLocation/WhatsMyLocation/ViewController.swift
+++ WhatsMyLocation/WhatsMyLocation/ViewController.swift
@@ -8,52 +8,66 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
-    @IBOutlet weak var latitude: UILabel!
-    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
 //    @IBOutlet weak var distance: UILabel!
     
-    var locationManager: CLLocationManager = CLLocationManager()
-    var startLocation: CLLocation!
+    
+    let locationManager = CLLocationManager()
+//    var startLocation = CLLocation()
 
 //    @IBAction func resetDistance(_ sender: AnyObject) {
 //        startLocation = nil
 //    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        startLocation = nil
-    }
 
-    func locationManager(_ manager: CLLocationManager,
-                         didUpdateLocations locations: [CLLocation])
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
-        let latestLocation: CLLocation = locations[locations.count - 1]
+        let location = locations[0]
         
-        latitude.text = String(format: "%.4f",
-                               latestLocation.coordinate.latitude)
-        longitude.text = String(format: "%.4f",
-                                latestLocation.coordinate.longitude)
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.10, 0.10)
         
-        if startLocation == nil {
-            startLocation = latestLocation
-        }
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        mapView.setRegion(region, animated: true)
+        
+        self.mapView.showsUserLocation = true
+        
+    }
+    
+    
+//        let latestLocation: CLLocation = locations[locations.count - 1]
+//        
+//        latitude.text = String(format: "%.4f",
+//                               latestLocation.coordinate.latitude)
+//        longitude.text = String(format: "%.4f",
+//                                latestLocation.coordinate.longitude)
+    
+//        if startLocation == nil {
+//            startLocation = latestLocation
         
 //        let distanceBetween: CLLocationDistance =
 //            latestLocation.distance(from: startLocation)
         
 //        distance.text = String(format: "%.2f", distanceBetween)
-    }
+//    }
     
-    func locationManager(_ manager: CLLocationManager,
-                         didFailWithError error: Error) {
+//    func locationManager(_ manager: CLLocationManager,
+//                         didFailWithError error: Error) {
+//        
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 }
